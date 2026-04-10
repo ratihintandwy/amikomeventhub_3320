7f2837bd3a0bf363fe97be39188567e9e58483c3
Faishal Mumtazah <faishal@students.amikom.ac.id>
2026-04-10 11:57:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
 });
 
 Route::get('/kontak', function () {
